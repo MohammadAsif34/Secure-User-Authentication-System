ab18dcb3e595bed36bb57db10555cada09fe4f60
MohammadAsif34 <shadowforever34@gmail.com>
2025-07-16 01:16:24 +0530

    update

--- Secure Auth System API/server.js
+++ Secure Auth System API/server.js
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URL)
