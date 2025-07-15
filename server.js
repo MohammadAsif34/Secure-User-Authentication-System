8826f208fee7a76e9325b5af4c60792c3115d163
MohammadAsif34 <shadowforever34@gmail.com>
2025-07-16 02:38:27 +0530

    update\

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://secure-auth-system-mohammadasif34.netlify.app/",
+    ],
     credentials: true,
   })
 );
